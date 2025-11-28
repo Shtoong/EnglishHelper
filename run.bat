f03ea56a0c584bdf51c4dcb2158cb7f5eec5202b
Shtoong <sergey@grebnev.com>
2025-11-28 19:49:24 +0200

    add check .venv

--- run.bat
+++ run.bat
@@ -2,9 +2,26 @@
 :: Переходим в папку, где лежит этот батник (чтобы запускать от имени админа или с ярлыка)
 cd /d "%~dp0"
 
+chcp 65001 >nul
+
+if not exist ".venv" (
+    echo Creating virtual environment...
+    python -m venv .venv
+)
+
+call .venv\Scripts\activate.bat
+
+if not exist ".venv\Lib\site-packages\pynput" (
+    echo Installing dependencies...
+    pip install -r requirements.txt
+)
+
+echo Starting EnglishHelper...
+
+
 :: Запускаем скрипт через pythonw.exe из папки venv
 :: pythonw (с буквой w) нужен, чтобы не висело черное окно консоли
-start "" "venv\Scripts\pythonw.exe" "main.pyw"
+start "" ".venv\Scripts\pythonw.exe" "main.pyw"
 
 exit
 
